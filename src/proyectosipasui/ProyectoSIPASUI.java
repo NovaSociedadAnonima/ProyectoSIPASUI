98abc8b62e5c8d1fef0a0c7150440de9f5cbf7f8
Fabri <fabuz313@gmail.com>
2021-11-07 18:57:24 -0300

    si

--- src/proyectosipasui/ProyectoSIPASUI.java
+++ src/proyectosipasui/ProyectoSIPASUI.java
@@ -11,7 +11,7 @@ package proyectosipasui;
  */
 public class ProyectoSIPASUI {
     
-    private static         UI_Main inicio = new UI_Main();
+    private static UI_Main inicio = new UI_Main();
 
     /**
      * @param args the command line arguments
