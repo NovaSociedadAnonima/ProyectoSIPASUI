99508d208f78025f86aea44242a258372bbb56c6
unknown <fabuz313@gmail.com>
2021-11-18 10:17:31 -0300

    Aconsejado por la profe

--- src/proyectosipasui/agregandonuevoprofesor.java
+++ src/proyectosipasui/agregandonuevoprofesor.java
@@ -117,6 +117,7 @@ public class Agregandonuevoprofesor extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private static Profesor profesor = new Profesor();
+   
     public static ArrayList<Profesor> Profesores = new ArrayList<>();
     
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
@@ -137,39 +138,6 @@ public class Agregandonuevoprofesor extends javax.swing.JFrame {
     
     private void centrar(){
       setLocationRelativeTo(null);
-    }
-  
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Agregandonuevoprofesor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Agregandonuevoprofesor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Agregandonuevoprofesor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Agregandonuevoprofesor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new Agregandonuevoprofesor().setVisible(true);
-            }
-        });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
